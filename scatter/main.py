2e0830fc361f81d8b4080e3437baef5a67a83d06
Abhijit Roy <dalvath3700@gmail.com>
2021-02-28 17:48:19 +0530

    scatter plot added

---
+++ scatter/main.py
@@ -0,0 +1,63 @@
+import plotly as py
+import plotly.graph_objs as go
+
+"""
+"Description": This function plots Scatter Chart with multiple (>= 1) trace(s) v/s Date.
+"x": Date
+"y1": trace-1
+"y2": trace-2
+"y3": trace-3
+"""
+def draw_scatter_plot(
+            x, 
+            y1, 
+            y2, 
+            remarks
+    ):
+    trace1 = go.Scatter(
+            x = x,
+            y = y1,
+            name = 'delta-CP',
+            mode = 'lines+markers',
+            line = dict(
+                    color = line_color,
+                    width = 2),
+            marker = dict(
+                    color = marker_color,
+                    size = 8,
+                    line = dict(
+                        color = marker_border_color,
+                        width = 0.5),
+                    ),
+            text = remarks
+    )
+
+    trace2 = go.Scatter(
+            x = x,
+            y = y2,
+            name = 'USL',
+            mode = 'lines',
+            line = dict(
+                    color = sl_color,
+                    width = 3)
+    )
+
+    trace3 = go.Scatter(
+            x = x,
+            y = y3,
+            name = 'UCL',
+            mode = 'lines',
+            line = dict(
+                    color = cl_color,
+                    width = 3)
+    )
+
+    data = [trace1, trace2, trace3]
+
+    layout = dict(
+            title = cp_plot_title,
+            xaxis = dict(title= cp_plot_xlabel),
+            yaxis = dict(title= cp_plot_ylabel)
+        )
+    fig = dict(data= data, layout= layout)
+    py.offline.plot(fig, filename= cp_plot_html_file)
